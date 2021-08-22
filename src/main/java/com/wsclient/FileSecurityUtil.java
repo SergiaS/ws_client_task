1045ca42d6d33e4923881fa9fe219eee61f723e4
sergias <sergias@ukr.net>
2021-08-22 15:06:09 +0300

    Added ability to encode and compress websocket data to file from one stream. Added ability to decode and decompress file.

--- src/main/java/com/wsclient/FileSecurityUtil.java
+++ src/main/java/com/wsclient/FileSecurityUtil.java
@@ -3,33 +3,54 @@ package com.wsclient;
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
-import java.util.zip.ZipOutputStream;
+import java.util.zip.*;
 
 public final class FileSecurityUtil {
 
     private static final String ALGORITHM = "AES";
-    private static final byte[] KEY_VALUE = new byte[]{'0', '2', '3', '4', '5', '6', '7', '9', '8', '2', '3', '4', '6', '7', '9', '8'};
+    private static final byte[] KEY_VALUE = new byte[]{'0','2','3','4','5','6','7','9','8','2','3','4','6','7','9','8'};
 
     public static void main(String[] args) throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
-//        encryptFile("20210820-105840_2.txt");
-        decryptFileDataByLine("20210820-121957.txt");
-//        compressFile(sourceFile);
-//        decompressFileData("compressed.txt");
+//        encryptFile("20210822-125923_2.txt");
+//        decryptFile("20210822-110717_2_enc2.txt");
+
+//        gzipFile("20210822-110717_2.txt");
+//        ungzipFile("comp_gzip.txt");
+
+        decompressAndDecrypt("20210822-141454.txt");
     }
 
+    private static void decompressAndDecrypt(String filename) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException {
+        Key key = new SecretKeySpec(KEY_VALUE, ALGORITHM);
+        Cipher cipherInstance = Cipher.getInstance(ALGORITHM);
+        cipherInstance.init(Cipher.DECRYPT_MODE, key);
+        try (FileInputStream fis = new FileInputStream(filename);
+             CipherInputStream cis = new CipherInputStream(fis, cipherInstance);
+             GZIPInputStream gis = new GZIPInputStream(cis);
+        ) {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(gis, StandardCharsets.UTF_8));
+            StringBuilder res = new StringBuilder();
+            String sTmp;
+            while ((sTmp = bufferedReader.readLine()) != null) {
+                res.append(sTmp).append("\n");
+            }
+            System.out.println(res);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     private static void encryptFile(String filename) {
-        try {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename));) {
             Key key = new SecretKeySpec(KEY_VALUE, ALGORITHM);
             Cipher cipher = Cipher.getInstance(ALGORITHM);
             cipher.init(Cipher.ENCRYPT_MODE, key);
-
             CipherOutputStream cipherOut = new CipherOutputStream(new FileOutputStream(filename.replace(".txt", "_enc2.txt")), cipher);
-            BufferedReader reader = new BufferedReader(new FileReader(filename));
 
             String line;
             while ((line = reader.readLine()) != null) {
@@ -42,7 +63,7 @@ public final class FileSecurityUtil {
         }
     }
 
-    private static void decryptFileDataByLine(String filename) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+    private static void decryptFile(String filename) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
         Key key = new SecretKeySpec(KEY_VALUE, ALGORITHM);
         Cipher cipherInstance = Cipher.getInstance(ALGORITHM);
         cipherInstance.init(Cipher.DECRYPT_MODE, key);
@@ -62,7 +83,36 @@ public final class FileSecurityUtil {
         }
     }
 
-    private static void compressFile(String sourceFile) throws IOException {
+    private static void gzipFile(String filename) {
+        try (FileInputStream fis = new FileInputStream(filename);
+             FileOutputStream fos = new FileOutputStream("comp_gzip.txt");
+             GZIPOutputStream gos = new GZIPOutputStream(fos);) {
+
+            byte[] buffer = new byte[1024];
+            for (int len; (len = fis.read(buffer)) != -1; ) {
+                gos.write(buffer, 0, len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void ungzipFile(String filename) {
+        try (FileInputStream fis = new FileInputStream(filename);
+             GZIPInputStream gis = new GZIPInputStream(fis);
+             FileOutputStream fos = new FileOutputStream("uncomp_gzip.txt");) {
+
+            byte[] bytes = new byte[1024];
+            for (int len; (len = gis.read(bytes)) > 0; ) {
+                fos.write(bytes, 0, len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void compressFileByZip(String sourceFile) throws IOException {
         FileOutputStream fos = new FileOutputStream("compressed.txt");
         ZipOutputStream zipOut = new ZipOutputStream(fos);
         File fileToZip = new File(sourceFile);
@@ -80,7 +130,7 @@ public final class FileSecurityUtil {
         fos.close();
     }
 
-    private static void decompressFileData(String filename) throws IOException {
+    private static void decompressFileByZip(String filename) throws IOException {
         ZipInputStream zis = new ZipInputStream(new FileInputStream(filename));
         ZipEntry zipEntry = zis.getNextEntry();
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -99,8 +149,8 @@ public final class FileSecurityUtil {
             throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException,
             BadPaddingException, IllegalBlockSizeException {
 
-        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-        SecretKey secretKey = new SecretKeySpec(keyBytes, "AES");
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        SecretKey secretKey = new SecretKeySpec(keyBytes, ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
         return cipher.doFinal(message);
     }
--- src/main/java/com/wsclient/TestApp.java
+++ src/main/java/com/wsclient/TestApp.java
@@ -1,10 +1,8 @@
 package com.wsclient;
 
-import com.wsclient.model.RequestCommand;
 import com.wsclient.websocket.WebSocketClientEndpoint;
 
 import javax.crypto.Cipher;
-import javax.crypto.CipherInputStream;
 import javax.crypto.CipherOutputStream;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
@@ -16,7 +14,7 @@ import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
+import java.util.zip.GZIPOutputStream;
 
 // SUBSCRIBE;aggTrade;btcusdt
 public class TestApp {
@@ -36,9 +34,9 @@ public class TestApp {
             e.printStackTrace();
         }
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-             BufferedWriter writer = new BufferedWriter(new FileWriter(dtf.format(LocalDateTime.now()) + "_2" + ".txt"));
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(dtf.format(LocalDateTime.now()) + "_2" + ".txt"));
              CipherOutputStream cipherOut = new CipherOutputStream(new FileOutputStream(filename), cipher);
+             GZIPOutputStream gzip = new GZIPOutputStream(cipherOut);
         ) {
             WebSocketClientEndpoint clientEndpoint = new WebSocketClientEndpoint(new URI(baseEndpoint));
 
@@ -46,7 +44,7 @@ public class TestApp {
                 @Override
                 public void handleMessage(String message) {
                     try {
-                        cipherOut.write((message + "\n").getBytes());
+                        gzip.write((message + "\n").getBytes());
 
                         writer.write(message + "\n");
                     } catch (IOException e) {
@@ -56,49 +54,49 @@ public class TestApp {
                 }
             });
 
-//            String json = "{\n" +
-//                    "\"method\": \"SUBSCRIBE\",\n" +
-//                    "\"params\":\n" +
-//                    "[\n" +
-//                    "\"btcusdt@aggTrade\"\n" +
-//                    "],\n" +
-//                    "\"id\": 1\n" +
-//                    "}";
-//            clientEndpoint.sendMessage(json);
-//            try {
-//                Thread.sleep(3000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-
-            String command;
-            while (!(command = reader.readLine()).equals("stop")) {
-                RequestCommand requestCommand;
-                if (command.contains("SUBSCRIBE")) {
-                    String[] splitCommands = command.split(";");
-                    long count = Arrays.stream(splitCommands)
-                            .filter(TestApp::commandValidation).count();
-                    if (count != 3) {
-                        wrongInput();
-                        continue;
-                    }
-                    requestCommand = new RequestCommand(splitCommands[0], splitCommands[1], splitCommands[2].toLowerCase());
-                } else {
-                    wrongInput();
-                    continue;
-                }
-
-                String jsonObj = "" +
-                        "{\n" +
-                            "\"method\": \"" + requestCommand.getAction() + "\",\n" +
-                            "\"params\":\n" +
-                            "[\n" +
-                            "\"" + requestCommand.getSymbol() + "@" + requestCommand.getChannel() + "\"\n" +
-                            "],\n" +
-                            "\"id\": " + (requestCommand.getAction().equalsIgnoreCase("SUBSCRIBE") ? 1 : 3) + "\n" +
-                        "}";
-                clientEndpoint.sendMessage(jsonObj);
+            String json = "{\n" +
+                    "\"method\": \"SUBSCRIBE\",\n" +
+                    "\"params\":\n" +
+                    "[\n" +
+                    "\"btcusdt@aggTrade\"\n" +
+                    "],\n" +
+                    "\"id\": 1\n" +
+                    "}";
+            clientEndpoint.sendMessage(json);
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+
+//            String command;
+//            while (!(command = reader.readLine()).equals("stop")) {
+//                RequestCommand requestCommand;
+//                if (command.contains("SUBSCRIBE")) {
+//                    String[] splitCommands = command.split(";");
+//                    long count = Arrays.stream(splitCommands)
+//                            .filter(TestApp::commandValidation).count();
+//                    if (count != 3) {
+//                        wrongInput();
+//                        continue;
+//                    }
+//                    requestCommand = new RequestCommand(splitCommands[0], splitCommands[1], splitCommands[2].toLowerCase());
+//                } else {
+//                    wrongInput();
+//                    continue;
+//                }
+//
+//                String jsonObj = "" +
+//                        "{\n" +
+//                            "\"method\": \"" + requestCommand.getAction() + "\",\n" +
+//                            "\"params\":\n" +
+//                            "[\n" +
+//                            "\"" + requestCommand.getSymbol() + "@" + requestCommand.getChannel() + "\"\n" +
+//                            "],\n" +
+//                            "\"id\": " + (requestCommand.getAction().equalsIgnoreCase("SUBSCRIBE") ? 1 : 3) + "\n" +
+//                        "}";
+//                clientEndpoint.sendMessage(jsonObj);
+//            }
         } catch (URISyntaxException | IOException e) {
             e.printStackTrace();
         }
